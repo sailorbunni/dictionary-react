116ee13b555f2e4bb9d05343c9f7805a75987be6
sailorbunni <92497221+sailorbunni@users.noreply.github.com>
2021-12-05 20:03:24 -0600

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -9,11 +10,10 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {" "}
-              {definition.definition} <br />
-              <em>{definition.example}</em>{" "}
-            </p>{" "}
+            {" "}
+            <strong>Definition:</strong> {definition.definition} <br />
+            <em>ie: {definition.example}</em> <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
---
+++ src/Synonyms.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms.length) {
+    return (
+      <div>
+        {" "}
+        <strong>Synonyms: </strong>
+        <ul className="Synonyms">
+          {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}

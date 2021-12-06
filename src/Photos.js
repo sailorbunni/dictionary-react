0725f1a8df2434bd9e1055ee6b02a5e42d206c09
sailorbunni <92497221+sailorbunni@users.noreply.github.com>
2021-12-06 01:17:46 -0600

    removed title

--- src/Photos.js
+++ src/Photos.js
@@ -8,12 +8,7 @@ export default function Photos(props) {
         {props.photos.map(function (photo, index) {
           return (
             <div className="col-4" key={index}>
-              <a
-                href={photo.src.original}
-                target="_blank"
-                rel="noreferrer"
-                title={props.alt}
-              >
+              <a href={photo.src.original} target="_blank" rel="noreferrer">
                 <img src={photo.src.landscape} className="img-fluid" />
               </a>
             </div>
